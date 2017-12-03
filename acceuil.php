7af2187f4cc31ab216e5ca74ff8190c09058ebba
wiem <wiembrahmi.enis@gmail.com>
2017-12-03 12:34:12 +0100

    full project

---
+++ acceuil.php
@@ -0,0 +1,330 @@
+
+<html>
+<head> 
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>devFest </title>
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/flexslider.css">
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/responsive.css">
+<link rel="stylesheet" href="css/animate.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+<script type="text/javascript" async="async" defer="defer" data-cfasync="false" src="https://mylivechat.com/chatinline.aspx?hccid=83497985"></script>
+
+<meta name="description" content="">
+<meta name="author" content="">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="css2/bootstrap.min.css">
+<link rel="stylesheet" href="css2/animate.css">
+<link rel="stylesheet" href="css2/font-awesome.min.css">
+<link rel="stylesheet" href="css2/owl.theme.css">
+<link rel="stylesheet" href="css2/owl.carousel.css">
+<link rel="stylesheet" href="css2/pub.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css2/style.css">
+
+
+<!-- Video et feed back -->
+<meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+  <meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+  <meta name="author" content="gettemplates.co" />
+
+    <!-- Facebook and Twitter integration -->
+  <meta property="og:title" content=""/>
+  <meta property="og:image" content=""/>
+  <meta property="og:url" content=""/>
+  <meta property="og:site_name" content=""/>
+  <meta property="og:description" content=""/>
+  <meta name="twitter:title" content="" />
+  <meta name="twitter:image" content="" />
+  <meta name="twitter:url" content="" />
+  <meta name="twitter:card" content="" />
+
+  <link href='https://fonts.googleapis.com/css?family=Raleway:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+  
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="css3/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="css3/icomoon.css">
+  <!-- Bootstrap  -->
+  <link rel="stylesheet" href="css3/bootstrap.css">
+
+  <!-- Magnific Popup -->
+  <link rel="stylesheet" href="css3/magnific-popup.css">
+
+  <!-- Owl Carousel  -->
+  <link rel="stylesheet" href="css3/owl.carousel.min.css">
+  <link rel="stylesheet" href="css3/owl.theme.default.min.css">
+
+  <!-- Theme style  -->
+  <link rel="stylesheet" href="css3/style.css">
+
+  <!-- Modernizr JS -->
+  <script src="js3/modernizr-2.6.2.min.js"></script>
+  <!-- FOR IE9 below -->
+  <!--[if lt IE 9]>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+
+
+<!-- Google Font -->
+</head>
+<body>
+  <!-- =========================
+     PRE LOADER       
+============================== -->
+<div class="preloader">
+
+  <div class="sk-rotating-plane"></div>
+
+</div>
+
+
+<!-- =========================
+     NAVIGATION LINKS     
+============================== -->
+<div class="navbar navbar-fixed-top custom-navbar" role="navigation">
+  <div class="container">
+
+    <!-- navbar header -->
+    <div class="navbar-header">
+      <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon icon-bar"></span>
+        <span class="icon icon-bar"></span>
+        <span class="icon icon-bar"></span>
+      </button>
+      <a href="#" class="navbar-brand">DevFest</a>
+    </div>
+
+    <div class="collapse navbar-collapse">
+
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#intro" class="smoothScroll">Intro</a></li>
+      
+
+        <li><a href="#contact" class="smoothScroll">Contact</a></li>
+      </ul>
+
+    </div>
+
+  </div>
+</div>
+
+
+<!-- =========================
+    INTRO SECTION   
+============================== -->
+<section id="intro" class="parallax-section">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-md-12 col-sm-12">
+        <h3 class="wow bounceIn" data-wow-delay="0.9s">December 2 - 3 in Sfax</h3>
+        <h1 class="wow fadeInUp" data-wow-delay="1.6s">hackathon</h1>
+        <!--<a href="#overview" class="btn btn-lg btn-default smoothScroll wow fadeInUp hidden-xs" data-wow-delay="2.3s">LEARN MORE</a> -->
+        <!--<a href="#register" class="btn btn-lg btn-danger smoothScroll wow fadeInUp" data-wow-delay="2.3s">REGISTER NOW</a>-->
+      </div>
+
+
+    </div>
+  </div>
+</section>
+<!-- Header Section --> 
+<!-- Slider Section -->	
+<section id="header-slider" class="section">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel"> 
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+    </ol>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active"> <img src="images/slider/hack.png" alt="Chania">
+        <div class="carousel-caption">
+          <h3>Actualities</h3>
+          <p></p>
+        </div>
+      </div>
+      <div class="item"> <img src="images/slider/acceuil.png" alt="Chania">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+    </div>
+    <!-- Controls --> 
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev"> <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span> <span class="sr-only">Previous</span></a> <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next"> <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> <span class="sr-only">Next</span></a></div>
+</section>
+<!-- Service Section --> 
+
+<!-- portfolio grid section -->
+<section id="portfolio" class="section portfolio">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-6 portfolio-item"> <a href="work-details.html" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>DevFest 2013</h3>
+           
+          </div>
+        </div>
+        <img src="images/portfolio/devfest2013.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="work-details.html" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>DevFest 2014</h3>
+          
+          </div>
+        </div>
+        <img src="images/portfolio/devfest2014.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="work-details.html" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>DevFest 2015</h3>
+            
+          </div>
+        </div>
+        <img src="images/portfolio/devfest2015.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="work-details.html" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>DevFest 2016</h3>
+            
+          </div>
+        </div>
+        <img src="images/portfolio/devfest2016.jpg" class="img-responsive" alt=""> </a> </div>
+      <div class="col-sm-6 portfolio-item"> <a href="work-details.html" class="portfolio-link">
+        <div class="caption">
+          <div class="caption-content">
+            <h3>devFest 2017</h3>
+           
+          </div>
+        </div>
+        <img src="images/portfolio/devfest2017.jpg" class="img-responsive" alt=""> </a> </div>
+      
+  </div>
+
+
+</section>
+  <div id="gtco-features-2">
+    <div class="gtco-container">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center gtco-heading">
+          <h2>Watch Our Videos</h2>
+          <p></p>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-4">
+          <div class="gtco-video gtco-video-sm gtco-bg" style="background-image: url(images3/img_1.jpg); ">
+            <a href="https://www.youtube.com/watch?v=rBCCfnzisTI" ><i class="icon-video"></i></a>
+            <div class="overlay"></div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gtco-video gtco-video-sm gtco-bg" style="background-image: url(images3/img_1.jpg); ">
+            <a href="https://www.youtube.com/watch?v=wElXPEpZY60" ><i class="icon-video"></i></a>
+            <div class="overlay"></div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="gtco-video gtco-video-sm gtco-bg" style="background-image: url(images3/img_1.jpg); ">
+            <a href="https://www.youtube.com/watch?v=QamsFfs_KaY" ><i class="icon-video"></i></a>
+            <div class="overlay"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+<!-- =========================
+    CONTACT SECTION   
+============================== -->
+
+<section id="contact" class="parallax-section">
+  <div class="container">
+    <div class="row">
+
+      <div class="wow fadeInUp col-md-offset-1 col-md-5 col-sm-6" data-wow-delay="0.6s">
+        <div class="contact_des">
+          <h4>If you have any question don't hesitate to contact us!</h4>
+        
+         
+        </div>
+      </div>
+
+      <div class="wow fadeInUp col-md-5 col-sm-6" data-wow-delay="0.9s">
+        <div class="contact_detail">
+          <div class="section-title">
+            <h2>Keep in touch</h2>
+          </div>
+          <form action="#" method="post">
+            <input name="name" type="text" class="form-control" id="name" placeholder="Name">
+              <input name="email" type="email" class="form-control" id="email" placeholder="Email">
+              <textarea name="message" rows="5" class="form-control" id="message" placeholder="Message"></textarea>
+            <div class="col-md-6 col-sm-10">
+              <input name="submit" type="submit" class="form-control" id="submit" value="SEND">
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
+
+
+-->
+
+<!-- Back top 
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+
+<!-- =========================
+     SCRIPTS   
+============================== -->
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.parallax.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+<!-- JS FILES --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> 
+<script src="js2/bootstrap.min.js"></script> 
+<script src="js2/jquery.flexslider-min.js"></script> 
+<script src="js2/retina.min.js"></script> 
+<script src="js2/modernizr.js"></script> 
+<script src="js2/main.js"></script>
+<!-- jQuery -->
+  <script src="js3/jquery.min.js"></script>
+  <!-- jQuery Easing -->
+  <script src="js3/jquery.easing.1.3.js"></script>
+  <!-- Bootstrap -->
+  <script src="js3/bootstrap.min.js"></script>
+  <!-- Waypoints -->
+  <script src="js3/jquery.waypoints.min.js"></script>
+  <!-- Carousel -->
+  <script src="js3/owl.carousel.min.js"></script>
+  <!-- countTo -->
+  <script src="js3/jquery.countTo.js"></script>
+  <!-- Magnific Popup -->
+  <script src="js3/jquery.magnific-popup.min.js"></script>
+  <script src="js3/magnific-popup-options.js"></script>
+  <!-- Main -->
+  <script src="js3/main.js"></script>
+
+
+</body>
+</html>
